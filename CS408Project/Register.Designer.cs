013c61ef605f96d3ce04d6a1b188658bac7759ae
Tanner Summers <Tannersummers@gmail.com>
2016-05-18 22:57:48 -0700

    General Backup 2

--- CS408Project/Register.Designer.cs
+++ CS408Project/Register.Designer.cs
@@ -58,12 +58,16 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
             this.panel1.Controls.Add(this.radioButton2);
             this.panel1.Controls.Add(this.textBox10);
             this.panel1.Controls.Add(this.label14);
@@ -330,6 +334,24 @@
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(884, 509);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(253, 137);
+            this.button1.TabIndex = 28;
+            this.button1.Text = "Create";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1297, 500);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(253, 137);
+            this.button2.TabIndex = 29;
+            this.button2.Text = "Cancel";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Register
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
@@ -380,5 +402,7 @@
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
     }
 }
